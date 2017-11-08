9424e4ef6afad042f54600e824625b4a621a0f15
Robert Todea <robert.todea@e-spres-oh.com>
2017-11-08 22:33:42 +0200

    Initial commit from a ZIP created by http://play.nativescript.org

---
+++ app/home/home.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui-pro/sidedrawer/angular";
+import { NativeScriptUIListViewModule } from "nativescript-telerik-ui-pro/listview/angular";
+import { NativeScriptUICalendarModule } from "nativescript-telerik-ui-pro/calendar/angular";
+import { NativeScriptUIChartModule } from "nativescript-telerik-ui-pro/chart/angular";
+import { NativeScriptUIDataFormModule } from "nativescript-telerik-ui-pro/dataform/angular";
+import { NativeScriptUIAutoCompleteTextViewModule } from "nativescript-telerik-ui-pro/autocomplete/angular";
+import { NativeScriptUIGaugesModule } from "nativescript-telerik-ui-pro/gauges/angular";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
+
+@NgModule({
+    imports: [
+        NativeScriptUISideDrawerModule,
+        NativeScriptUIListViewModule,
+        NativeScriptUICalendarModule,
+        NativeScriptUIChartModule,
+        NativeScriptUIDataFormModule,
+        NativeScriptUIAutoCompleteTextViewModule,
+        NativeScriptUIGaugesModule,
+        NativeScriptModule,
+        HomeRoutingModule,
+        NativeScriptFormsModule
+    ],
+    declarations: [
+        HomeComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class HomeModule { }
