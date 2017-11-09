5a07d8fd26a7a8fb6f426cf174af02c0a27f864f
Robert Todea <robert.todea@e-spres-oh.com>
2017-11-09 09:04:55 +0200

    Hiding the Status bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Page } from 'tns-core-modules/ui/page';
+import statusBar = require("nativescript-status-bar");
+
 
 @Component({
     selector: "ns-app",
@@ -9,6 +11,8 @@ export class AppComponent implements OnInit {
     constructor(private page: Page) {}
 
     ngOnInit() {
+        // The ActionBar is different than the StatusBar
         // this.page.actionBarHidden = true;
+        statusBar.hide();
     }
 }
