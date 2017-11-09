315f7c5be34a1149178e5069f016dbba99f76464
Robert Todea <robert.todea@e-spres-oh.com>
2017-11-09 08:16:27 +0200

    More on the initial commit

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { EventData } from 'tns-core-modules/data/observable';
+import { Label } from 'tns-core-modules/ui/label';
 
 @Component({
     selector: "Home",
@@ -8,9 +10,25 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
+    public oneway = "One way bound label";
+    public twoway = "Two way bound label";
+    public counter: number;
+
     constructor() {
+        this.counter = 0;
     }
 
-    ngOnInit(): void {
+    changeLabelText() {
+        this.twoway += " Two way bound label ";
+        this.counter += 1;
     }
+
+    onTextChanged(args: EventData) {
+        let label = <Label>args.object;
+        console.log("onTextChanged for " + this.counter + " times for element " + label);
+    }
+
+    ngOnInit() {
+    }
+
 }
