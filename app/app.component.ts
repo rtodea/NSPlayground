105ab4c1b1fe7a23f871c32e8cbd5112635fd1e4
Robert Todea <robert.todea@e-spres-oh.com>
2017-11-09 08:39:50 +0200

    Playing with status-bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Page } from 'tns-core-modules/ui/page';
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    constructor(private page: Page) {}
+
+    ngOnInit() {
+        // this.page.actionBarHidden = true;
+    }
+}
